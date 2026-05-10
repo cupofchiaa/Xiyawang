a8ec04ea5328f334aa2487f8936366eaef4d6ec6
Maxime Kawawa-Beaudan <maxime.jkb@gmail.com>
2026-05-10 15:50:01 -0400

    initial

---
+++ config.js
@@ -0,0 +1,11 @@
+const WORKS = [
+  // {
+  //   title: "",
+  //   medium: "",
+  //   dimensions: '',
+  //   category: "painting", // or "printmaking"
+  //   image: "images/",
+  //   originalAvailable: true,
+  //   shopifyProductId: "",
+  // },
+];
